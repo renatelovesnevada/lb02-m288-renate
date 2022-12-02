0b5fa2cef30ebe9c853e42b01158c0108543081a
renatefirlie <firlier@bzz.ch>
2022-12-02 14:00:29 +0100

    update to js

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const dropDown = document.querySelectorAll(".dropdown select");
+fromCurrency = document.querySelector(".from select");
+toCurrency = document.querySelector(".to select");
+getButton = document.querySelectorAll("form button");
+
+//creating option tags
+for (let i = 0; i < dropDown.length; i++) {
+    for(currency_code in country_code){
+        let selected;
+        // Default
+        if(i == 0){
+            selected = currency_code == "CHF" ? "selected" : "";
+        } else if (i == 1) {
+            selected = currency_code == "GBP" ? "selected" : "";
+        }
+
+
+        let optionTag = '<option value="';
+            optionTag += currency_code;
+            optionTag += selected;
+            optionTag += '">';
+            optionTag += currency_code;
+            optionTag += '</option>';
+
+            // <option value="GBP">GBP</option>
+
+
+        //alert(optionTag);
+
+        dropDown[i].insertAdjacentHTML("beforeend", optionTag);
+    }
+}
+getButton[0].addEventListener("click", e => {
+    e.preventDefault(); //STop fr
+    getExchangeRate();
+});
+
+function getExchangeRate(){
+    const amount = document.querySelector(". amount input");
+    let amountVal = amount.value;
+    if(amountVal == "" || amountVal == "0" ){
+        amount.value = "1";
+        amount.value = 1;
+        //Default is 1, if under 1 it will change to 1 automatically
+    }
+    let url = 'https://v6.exchangerate-api.com/v6/3aa0b0ecf93cec23872e6d52/latest/';
+        url += fromCurrency.value;
+    fetch(url).then(response => console.log(response.json()));
+}
